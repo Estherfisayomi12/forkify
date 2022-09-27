b71797e5368518782e3a02745454b8dcae44211e
Estherfisayomi12 <fisayomi2602@gmail.com>
2022-09-27 19:14:07 +0100

    my first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a6560e0e-cf46-4126-859b-ea83db17d8c7';
+export const MODAL_CLOSE_SEC = 2.5;
